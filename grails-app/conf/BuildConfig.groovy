8d00ea4261159dd7dde6c26091895159b4319fa6
Tomasz Przybysz <tpr@touk.pl>
2013-02-26 21:22:58 +0100

    Adjusted .gitignore, without those couple WEB-INF files it didn't run

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,9 +39,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion" {
-            exte
-        }
+        // runtime ":hibernate:$grailsVersion" // not necessary?
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
 
