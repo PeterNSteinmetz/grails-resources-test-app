b619048d500166a3eb41d7435b9279867404be1b
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2013-03-03 15:08:44 -0700

    Configuration which at least loads and runs pages. Tests not correct.

--- test/functional/LocalSpecPageSpec.groovy
+++ test/functional/LocalSpecPageSpec.groovy
@@ -1,9 +1,20 @@
+import geb.spock.GebReportingSpec
+import pages.*
+
 /**
- * Created with IntelliJ IDEA.
- * User: peter
+ * Spock test specification for tests of localSpec page.
+ *
+ * @author Peter N. Steinmetz
  * Date: 3/3/13
  * Time: 2:27 PM
- * To change this template use File | Settings | File Templates.
  */
-class LocalSpecPageSpec {
+class LocalSpecPageSpec extends GebReportingSpec {
+
+  def "check title"() {
+    when:
+    to LocalSpecPage
+
+    then:
+    title.contains("extra")
+  }
 }
